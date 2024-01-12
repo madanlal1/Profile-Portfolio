fa5ce27195d62ecf314c73217296406d0b5cb3d5
madanlal1 <madanlal.pk99@gmail.com>
2024-01-12 12:15:35 +0500

    new skills added

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,23 +1,23 @@
 export const experienceData = [
     {
         id: 1,
-        company: 'EduTrainia',
-        jobtitle: 'Software Engineer',
-        startYear: '2022',
-        endYear: '2023'
+        company: 'Yadgar Safar',
+        jobtitle: 'Frontend Developer',
+        startYear: 'Aug 2023',
+        endYear: 'Present'
     },
     {
         id: 2,
-        company: 'Yadgar Safar',
-        jobtitle: 'MERN Stack Developer',
-        startYear: '2021',
-        endYear: '2022'
+        company: 'EduTrainia',
+        jobtitle: 'Software Engineer',
+        startYear: 'July 2022',
+        endYear: 'June 2023'
     },
     {
         id: 3,
         company: 'Public Speaking & Literary Society',
         jobtitle: 'Volunteer',
-        startYear: '2019',
-        endYear: '2020'
+        startYear: 'Sep 2019',
+        endYear: 'Mar 2020'
     },
 ]
