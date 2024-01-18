a2d491345680e5c4471c2969a6899e69991f4ded
Madan Lal <81483460+madanlal1@users.noreply.github.com>
2024-01-18 14:06:28 +0500

    Delete src directory

--- src/data/contactsData.js
+++
@@ -1,7 +0,0 @@
-export const contactsData = {
-    email: 'madanlal.pk@gmail.com',
-    phone: '+923473836151',
-    address: 'Karachi, Sindh, Pakistan.',
-
-    sheetAPI: ''
-}
--- src/data/socialsData.js
+++
@@ -1,9 +0,0 @@
-export const socialsData = {
-    github: 'https://github.com/madanlal1',
-    facebook: 'https://www.facebook.com/madanlalls/',
-    linkedIn: 'https://www.linkedin.com/in/madanlal1',
-    instagram: 'https://www.instagram.com/ls_madan2/',
-    twitter: 'https://twitter.com/ls_madan2',
-    gitlab: 'https://gitlab.com/madanlal1',
-    youtube: 'https://www.youtube.com/channel/UCRcMl3tMMxnNbe8IRt0deJQ'
-}
