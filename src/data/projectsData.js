c9a95d7a1e6f25a77c8e95867abb9c6862aa445f
madanlal1 <madanlal.pk99@gmail.com>
2025-10-04 01:42:03 +0500

    updated portfolio site

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -11,6 +11,13 @@ import tenth from '../assets/svg/projects/tenth.png'
 import darosoft from '../assets/svg/projects/darosoft.png'
 import alifa from '../assets/svg/projects/alifa.png'
 import easydatastructures from '../assets/svg/projects/easydatastructures.png'
+import bookory from '../assets/svg/projects/bookory.webp'
+import acthire from '../assets/svg/projects/acthire.png'
+import fastmotors from '../assets/svg/projects/fastmotors.webp'
+import emerald from '../assets/svg/projects/emerald.webp'
+import sotech from '../assets/svg/projects/sotech.png'
+import tma from '../assets/svg/projects/tma.png'
+import moyenxpress from '../assets/svg/projects/moyenxpress.gif'
 
 
 
@@ -19,7 +26,7 @@ export const projectsData = [
         id: 1,
         projectName: 'DaroSoft',
         projectDesc: 'DaroSoft, built on the WordPress CMS, is a dynamic software house offering professional website development services, delivering seamless and customizable digital solutions.',
-        tags: ['WordPress', 'CSS', 'JavaScript'],
+        tags: ['WordPress'],
         code: 'https://darosoft.com/',
         demo: 'https://darosoft.com/',
         image: darosoft
@@ -28,7 +35,7 @@ export const projectsData = [
         id: 2,
         projectName: 'The Alifa',
         projectDesc: 'The Alifa, built on the WordPress CMS, is a dynamic digital agency delivering expert marketing solutions to boost visibility, engagement, and growth.',
-        tags: ['WordPress', 'CSS', 'JavaScript'],
+        tags: ['WordPress'],
         code: 'https://thealifa.com/',
         demo: 'https://thealifa.com/',
         image: alifa
@@ -44,24 +51,60 @@ export const projectsData = [
     },
     {
         id: 4,
-        projectName: 'Easy Data Structures',
-        projectDesc: '',
-        tags: ['React JS'],
-        code: 'https://github.com/madanlal1/Online-Code-Editor',
-        demo: 'https://madanlal1.github.io/Online-Code-Editor/',
-        image: one
+        projectName: 'Bookory Publishing',
+        projectDesc: 'Bookory is a WordPress WooCommerce project for authors, publishers, and bookstores to easily sell books and eBooks online.',
+        tags: ['WordPress'],
+        code: 'https://trotsoft.com/',
+        demo: 'https://trotsoft.com/',
+        image: bookory
     },
-
-
-
-
-
-
-
-
-
     {
-        id: 1,
+        id: 5,
+        projectName: 'Act Hire',
+        projectDesc: 'ACT Hire designed in WordPress, it delivers reliable dry hire solutions for the civil construction and earthmoving industries.',
+        tags: ['WordPress'],
+        code: 'https://acthire.com/',
+        demo: 'https://acthire.com/',
+        image: acthire
+    },
+    {
+        id: 6,
+        projectName: 'Fast Motors',
+        projectDesc: 'Fast Motors, a Karachi-based dealership for ISUZU, FUSO, and FOTON vehicles, parts, and services — website built with WordPress CMS development.',
+        tags: ['WordPress'],
+        code: 'https://fast-motors.com/',
+        demo: 'https://fast-motors.com/',
+        image: fastmotors
+    },
+    {
+        id: 7,
+        projectName: 'Emerald Propertiez',
+        projectDesc: 'EmeraldPropertiez is a WordPress-based real estate site offering property listings and services with a modern and intuitive interface.',    
+        tags: ['WordPress'],
+        code: 'https://emeraldpropertiez.com/',
+        demo: 'https://emeraldpropertiez.com/',
+        image: emerald
+    },
+    {
+        id: 8,
+        projectName: 'SoTech',
+        projectDesc: 'SoTech is a WordPress-based digital solution developed for The Alifa, offering modern web services and smart online presence.',    
+        tags: ['WordPress'],
+        code: 'https://client.thealifa.com/',
+        demo: 'https://client.thealifa.com/',
+        image: sotech
+    },
+    {
+        id: 9,
+        projectName: 'TMA',
+        projectDesc: 'Towel Association is an e-commerce site built on Shopify, offering products through a sleek, user-friendly online store.',    
+        tags: ['Shopify'],
+        code: 'https://towelassociation.myshopify.com/',
+        demo: 'https://towelassociation.myshopify.com/',
+        image: tma
+    },
+    {
+        id: 10,
         projectName: 'Online Code Editor',
         projectDesc: 'This online code editor, developed in HTML, CSS, and JavaScript, is a dynamic playground for coding enthusiasts, providing an effortless avenue to enhance coding skills.',
         tags: ['HTML', 'CSS', 'JavaScript'],
@@ -70,7 +113,7 @@ export const projectsData = [
         image: one
     },
     {
-        id: 2,
+        id: 11,
         projectName: 'Easy Data Structure Platform',
         projectDesc: 'Easy Data Structure offers animation, visualization, and definitions for various data structures.',
         tags: ['React', 'CSS', 'Bootstrap'],
@@ -79,7 +122,7 @@ export const projectsData = [
         image: two
     },
     {
-        id: 3,
+        id: 12,
         projectName: 'Google Keep Clone',
         projectDesc: 'I have crafted a detailed replica of Google Keep with concise yet functional HTML, CSS, and JavaScript code, achieving a balance between complexity and simplicity.',
         tags: ['HTML', 'CSS', 'JavaScript'],
@@ -88,7 +131,7 @@ export const projectsData = [
         image: three
     },
     {
-        id: 4,
+        id: 13,
         projectName: 'ToDo List App',
         projectDesc: 'A Simple E-commerce application',
         tags: ['React', 'CSS', 'Bootstrap'],
@@ -97,7 +140,7 @@ export const projectsData = [
         image: four
     },
     {
-        id: 5,
+        id: 14,
         projectName: 'Design Henge Website Clone',
         projectDesc: 'Design Henge is a creative digital agency providing expert design and development solutions to brands of all sizes.',
         tags: ['React', 'CSS', 'Bootstrap'],
@@ -106,7 +149,7 @@ export const projectsData = [
         image: five
     },
     {
-        id: 6,
+        id: 15,
         projectName: 'Yadgar Safar  Tourist Platform',
         projectDesc: 'Yadgar Safar is traveling platform whcih connects travellers and travels agencies together. where tourists can book tour of their choices by comparing different features of different travel agencies.',
         tags: ['React', 'NodeJS', 'ExpressJS', 'MongoDB'],
@@ -115,7 +158,7 @@ export const projectsData = [
         image: six
     },
     {
-        id: 7,
+        id: 16,
         projectName: 'Online Bakery Collection',
         projectDesc: 'Online Bakery Collection is an online platform for booking and delivering bakery orders.',
         tags: ['React', 'Bootstrap', 'MySQL'],
@@ -124,7 +167,7 @@ export const projectsData = [
         image: seven
     },
     {
-        id: 8,
+        id: 17,
         projectName: 'Hostel Management System',
         projectDesc: 'Hostel Management System Database Project is based on JAVA Programming which help to store hostel data.',
         tags: ['Java'],
@@ -133,7 +176,7 @@ export const projectsData = [
         image: eight
     },
     {
-        id: 9,
+        id: 18,
         projectName: 'PKR Digital Money Exchange',
         projectDesc: 'PKR Digital Money Exchange is developed in CPP Data Structure.',
         tags: ['C++'],
@@ -142,14 +185,23 @@ export const projectsData = [
         image: ninth
     },
     {
-        id: 10,
+        id: 19,
+        projectName: 'MoyenXpress',
+        projectDesc: 'MoyenXpress is an e-commerce platform built with HTML, CSS, JavaScript, and PHP, offering a seamless online shopping experience.',
+        tags: ['HTML', 'CSS', 'JavaScript'],
+        code: 'https://moyenxpress.com/',
+        demo: 'https://moyenxpress.com/',
+        image: moyenxpress
+    },
+    {
+        id: 20,
         projectName: 'Vehicle Parking Management System',
         projectDesc: 'Vehicle Parking Management is developed in C# language.',
         tags: ['C#'],
         code: 'https://github.com/madanlal1/Vehicle-Parking-Management-System-C_Sharp-Project',
         demo: 'https://github.com/madanlal1/Vehicle-Parking-Management-System-C_Sharp-Project',
         image: tenth
-    },
+    }
 ]
 
 
